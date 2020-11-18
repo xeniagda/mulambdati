78f1aca0ddbb2e674ac5c065b00094bb8290eaea
loovjo <jonathan.loov@gmail.com>
2020-11-18 11:51:33 +0100

    Factor read_state into read_state and render

--- static/handler.js
+++ static/handler.js
@@ -15,33 +15,46 @@ async function join_if_needed() {
 
 var last_combinators = undefined;
 
+var clstate = {
+    "selected_deck": -1,
+    "fv_name": "",
+};
+
+var data;
+
 async function read_state() {
     await join_if_needed();
 
+    let resp = await fetch('/api/state', {
+        method: 'GET',
+        credentials: 'same-origin',
+    });
+    data = await resp.json();
+}
+
+async function render() {
+    let left = render_player(data.game.players[0], true, data.you_are == 0);
+    let right = render_player(data.game.players[1], false, data.you_are == 1);
+
+    document.getElementById("players").innerHTML = "";
+    document.getElementById("players").appendChild(left);
+    document.getElementById("players").appendChild(right);
+
+    if (JSON.stringify(data.game.combinators) !== JSON.stringify(last_combinators)) {
+        let combinators = render_combinators(data.game.combinators)
+        document.getElementById("combinators").innerHTML = "";
+        combinators.forEach(e => document.getElementById("combinators").appendChild(e));
+
+        last_combinators = data.game.combinators;
+    }
+}
+
+async function render_loop() {
     while (true) {
-        let resp = await fetch('/api/state', {
-            method: 'GET',
-            credentials: 'same-origin',
-        });
-        let data = await resp.json();
-        console.log(data);
-
-        let left = render_player(data.game.players[0], true, data.you_are == 0);
-        let right = render_player(data.game.players[1], false, data.you_are == 1);
-
-        document.getElementById("players").innerHTML = "";
-        document.getElementById("players").appendChild(left);
-        document.getElementById("players").appendChild(right);
-
-        if (JSON.stringify(data.game.combinators) !== JSON.stringify(last_combinators)) {
-            let combinators = render_combinators(data.game.combinators)
-            document.getElementById("combinators").innerHTML = "";
-            combinators.forEach(e => document.getElementById("combinators").appendChild(e));
-
-            last_combinators = data.game.combinators;
-        }
+        await read_state();
+        await render();
         await new Promise(r => setTimeout(r, 250));
     }
 }
 
-read_state();
+render_loop();
