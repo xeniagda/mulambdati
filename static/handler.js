6754108a5eb9f2e1c9ba7e076e52a239f192c3ef
loovjo <jonathan.loov@gmail.com>
2020-11-18 11:28:31 +0100

    apparently async doesn't work too well with tail recursion :(

--- static/handler.js
+++ static/handler.js
@@ -18,29 +18,30 @@ var last_combinators = undefined;
 async function read_state() {
     await join_if_needed();
 
-    let resp = await fetch('/api/state', {
-        method: 'GET',
-        credentials: 'same-origin',
-    });
-    let data = await resp.json();
-    console.log(data);
+    while (true) {
+        let resp = await fetch('/api/state', {
+            method: 'GET',
+            credentials: 'same-origin',
+        });
+        let data = await resp.json();
+        console.log(data);
 
-    let left = render_player(data.game.players[0], true, data.you_are == 0);
-    let right = render_player(data.game.players[1], false, data.you_are == 1);
+        let left = render_player(data.game.players[0], true, data.you_are == 0);
+        let right = render_player(data.game.players[1], false, data.you_are == 1);
 
-    document.getElementById("players").innerHTML = "";
-    document.getElementById("players").appendChild(left);
-    document.getElementById("players").appendChild(right);
+        document.getElementById("players").innerHTML = "";
+        document.getElementById("players").appendChild(left);
+        document.getElementById("players").appendChild(right);
 
-    if (JSON.stringify(data.game.combinators) !== JSON.stringify(last_combinators)) {
-        let combinators = render_combinators(data.game.combinators)
-        document.getElementById("combinators").innerHTML = "";
-        combinators.forEach(e => document.getElementById("combinators").appendChild(e));
+        if (JSON.stringify(data.game.combinators) !== JSON.stringify(last_combinators)) {
+            let combinators = render_combinators(data.game.combinators)
+            document.getElementById("combinators").innerHTML = "";
+            combinators.forEach(e => document.getElementById("combinators").appendChild(e));
 
-        last_combinators = data.game.combinators;
+            last_combinators = data.game.combinators;
+        }
+        await new Promise(r => setTimeout(r, 250));
     }
-    await new Promise(r => setTimeout(r, 250));
-    await read_state(); // Hopefully stack doesn't overflow lol
 }
 
 read_state();
