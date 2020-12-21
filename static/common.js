39e580dd953644d5ffec0939b653b1210f6e1ea8
Jay Jayjay <jayjayjay@example.fios-router.home>
2020-12-20 20:32:02 -0500

    do stuff, i guess, i hope, don't mess up github

--- static/common.js
+++ static/common.js
@@ -5,3 +5,32 @@ function element_with_class_and_text(tag, className, content) {
     return element;
 }
 
+function switchMode() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    if (element.classList.contains("light-mode") && !(document.cookie.indexOf("lightmode=1") >= 0)) {
+        document.cookie = "lightmode=1"
+        console.log("Adding lightmode!")
+    } else if (!element.classList.contains("light-mode")) {
+        document.cookie = "lightmode=0"
+        console.log("Deleting lightmode!")
+    }
+}
+
+/*
+if( document.readyState !== 'loading' ) {
+  if(document.cookie.indexOf("lightmode=1;") >= 0) {
+    switchMode()
+  }
+} else {
+  document.addEventListener("DOMContentLoaded", function(event) { 
+    if(document.cookie.indexOf("lightmode=1;") >= 0) {
+      switchMode()
+    }
+  });
+}
+*/
+    if(document.cookie.indexOf("lightmode=1") >= 0) {
+      switchMode()
+      console.log("Initial Switch lightmode!")
+    }
